43f8b91567b67a40f01ebcefb443695cab06e685
brunobzki <bpanzacchi@gmail.com>
2024-10-10 21:22:14 -0300

    week 4

---
+++ routes/accountRoute.js
@@ -0,0 +1,11 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router()
+const utilities = require("../utilities"); 
+const accountController = require("../controllers/accountController")
+
+// Route to build inventory by classification view
+router.get("/login",accountController.buildLogin);
+
+
+module.exports = router;
